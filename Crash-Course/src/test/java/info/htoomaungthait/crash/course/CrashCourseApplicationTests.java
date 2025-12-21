990d9fab084190c7710120913ff5e5c3b40537cd
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-12-21 16:59:27 +0630

    frist repo initial commit

---
+++ Crash-Course/src/test/java/info/htoomaungthait/crash/course/CrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package info.htoomaungthait.crash.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
