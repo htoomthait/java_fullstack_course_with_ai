7ed91478dc5130f76f13cc86a19212301e068e6f
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-12-25 22:48:41 +0630

    updated react first lesson app

--- FE/my-frist-app/src/App.jsx
+++ FE/my-frist-app/src/App.jsx
@@ -6,6 +6,18 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const incrementCount = () => {
+    setCount(count + 5)
+  }
+
+  const decrementCount = () => {
+    setCount(count - 5)
+  }
+
+  const handleButtonClick = () => {
+    console.log("Button Clicked!")
+  }
+
   return (
     <>
       <div>
@@ -16,15 +28,26 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>My first react app</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={incrementCount} className=''>
+          count ++5
         </button>
+
+
+
+        <button onClick={decrementCount}>
+          count --5
+        </button>
+        <button onClick={handleButtonClick}> Click Me </button>
+        <p>Count is: {count}</p>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      <input type="text" placeholder="Type here..." onMouseEnter={() => console.log("Text Fields Entered!")} />
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
