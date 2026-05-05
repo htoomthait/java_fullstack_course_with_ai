fe5d583a1090c96ba1c789cf8bec8fc3d40dd339
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-05-05 21:38:54 +0630

    web client user list and get user by id can be done

--- buynowdotcom/src/main/java/net/htoomaungthait/buynowdotcom/controller/ExternalUserController.java
+++ buynowdotcom/src/main/java/net/htoomaungthait/buynowdotcom/controller/ExternalUserController.java
@@ -1,13 +1,19 @@
 package net.htoomaungthait.buynowdotcom.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import net.htoomaungthait.buynowdotcom.common.BaseController;
 import net.htoomaungthait.buynowdotcom.common.response.ApiResponse;
+import net.htoomaungthait.buynowdotcom.dto.resp.ExternalUserDto;
 import net.htoomaungthait.buynowdotcom.service.external.user.IExternalUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/external-users")
@@ -17,7 +23,7 @@ public class ExternalUserController extends BaseController {
     private final IExternalUserService externalUserService;
 
     @GetMapping("")
-    public ResponseEntity<ApiResponse<String>> getExternalUser(){
+    public ResponseEntity<ApiResponse<List<ExternalUserDto>>> getExternalUsers(){
 
         externalUserService.getListOfExternalUsers();
         return makeResponse(
@@ -25,7 +31,23 @@ public class ExternalUserController extends BaseController {
                 "EXT_USER_001",
                 "success",
                 getStatusMessageByCode("EXT_USER_001"),
-                "External User Data are logged in the console"
+                externalUserService.getListOfExternalUsers()
+        );
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse<ExternalUserDto>> getExternalUserById(
+            @PathVariable
+            @Valid
+            @Min(value = 1, message = "Id must be greater than or equal to 1")
+            Long userId
+    ){
+        return makeResponse(
+                200,
+                "EXT_USER_002",
+                "success",
+                getStatusMessageByCode("EXT_USER_002"),
+                externalUserService.getExternalUserById(userId)
         );
     }
 }
