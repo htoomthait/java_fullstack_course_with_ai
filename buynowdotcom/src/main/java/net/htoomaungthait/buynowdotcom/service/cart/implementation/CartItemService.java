432736ae9d178484a588a449a3f22c641c07f8ee
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-05-13 20:23:59 +0630

    update cart item lesson 89

--- buynowdotcom/src/main/java/net/htoomaungthait/buynowdotcom/service/cart/implementation/CartItemService.java
+++ buynowdotcom/src/main/java/net/htoomaungthait/buynowdotcom/service/cart/implementation/CartItemService.java
@@ -12,6 +12,8 @@ import net.htoomaungthait.buynowdotcom.service.cart.ICartService;
 import net.htoomaungthait.buynowdotcom.service.product.IProductService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @RequiredArgsConstructor
 public class CartItemService implements ICartItemService {
@@ -63,6 +65,22 @@ public class CartItemService implements ICartItemService {
 
     @Override
     public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+
+        cart.getItems().stream().filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+
+        BigDecimal totalAmount = cart.getItems().stream().map(CartItem ::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal :: add);
+
+        cart.setTotalAmount(totalAmount);
+
+        this.cartRepository.save(cart);
 
     }
 
