e387ed1287ec041f24cc962e3c7b5ac3c1e212ba
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-07-09 21:44:32 +0630

    lesson 121 done

--- fe-buynowdotcom/src/pages/Home.jsx
+++ fe-buynowdotcom/src/pages/Home.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hero from '../components/common/hero/Hero'
+import Paginator from '../components/common/Paginator';
+import { Card } from 'react-bootstrap';
 
 const Home = () => {
+    const [currentPage, setCurrentPage] = useState([]);
+    const itemPerPage = 10;
+
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    const indexOfLastProduct = currentPage * itemPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - itemPerPage;
+    const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+
+
     return (
         <>
-            <Hero />
+
             <div>
-                <h1>Welcome to Buy Now Dot Com</h1>
+                <Hero />
+
+                <Card className='home-product-card'>
+                    <Link to={"#"}> Product image will be here...</Link>
+                    <Card.Body>
+                        <p className="produt-description"> product name and description will be here. ...</p>
+
+                        <h4 className="price">product price...</h4>
+
+                        <p className="text-success"> Product inventory in stock</p>
+
+                        <Link to={"#"} className="shop-now-button"> Shop Now</Link>
+
+                    </Card.Body>
+                </Card>
+
+                <Paginator
+                    itemPerPage={itemPerPage}
+                    totalItems={filteredProducts.length}
+                    currentPage={currentPage}
+                    paginate={paginate}
+
+
+                />
             </div>
         </>
 
