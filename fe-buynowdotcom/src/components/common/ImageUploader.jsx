fad5aa03bc10f587ab51509286f11b9908b5402f
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-08-17 17:27:41 +0630

    lesson 171 done

--- fe-buynowdotcom/src/components/common/ImageUploader.jsx
+++ fe-buynowdotcom/src/components/common/ImageUploader.jsx
@@ -1,19 +1,55 @@
 import React, { useState } from 'react'
+import { nanoid } from 'nanoid';
+import { uploadImages } from '../../store/features/imageSlice';
+import { useDispatch } from 'react-redux';
+import { toast, ToastContainer } from 'react-toastify';
 
 const ImageUploader = ({ productId }) => {
     const [images, setImages] = useState([]);
+    const dispatch = useDispatch();
 
 
     const handleImageChange = (e) => {
         e.preventDefault();
         const files = Array.from(e.target.files);
 
-        const newImages = files.map((file) => {
+        const newImages = files.map((file) => ({
+            id: nanoid(),
+            name: file.name,
+            file
+
 
         })
+        );
 
-        //setImages(files);
+        setImages((prevImages) => [...prevImages, ...newImages]);
     }
+
+    const handleImageUpload = async (e) => {
+        e.prevImagesentDefault();
+
+        if (!productId) {
+            return;
+        }
+
+        if (Array.isArray(images) && images.length > 0) {
+            try {
+                const result = await dispatch(
+                    uploadImages({ productId, files: images.map((image) => image.file) })
+                ).unwrap();
+
+                clearFileInput();
+                toast.success('Images uploaded successfully');
+
+            } catch (error) {
+                toast.error('Failed to upload images');
+            }
+        }
+
+    }
+
+
+
     return (
         <div>ImageUploader</div>
     )
